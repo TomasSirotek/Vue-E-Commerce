c20d6a21971f0b1c6cb83d5857aee30c87afceec
TomasisBOSS <72190589+TomasisBOSS@users.noreply.github.com>
2021-10-09 17:43:10 +0200

    product edit mess

---
+++ src/components/Admin/EditModal.vue
@@ -0,0 +1,74 @@
+<template>
+  <b-modal id="bv-modal-example" hide-footer>
+    <template #modal-title>
+      Edit product  
+    </template>
+    <div class="d-block text-center">
+      <form class="user">
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="title"
+            v-model="productSection.title"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="category"
+            v-model="productSection.category"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="onSale"
+            v-model="productSection.onSale"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="desription"
+            v-model="productSection.desription"
+          />
+        </div>
+        <div class="form-group">
+          <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="In stock"
+            v-model="productSection.inStock"
+          />
+        </div>
+      </form>
+     
+    </div>
+    <b-button class="mt-3" block  @click="updateChanges"
+      >Safe options</b-button
+    >
+    <b-button class="mt-3" block @click="$bvModal.hide('bv-modal-example')"
+      >Close Me</b-button
+    >
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "EditModal",
+  methods: {
+    
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/Admin/OverviewTable.vue
+++ src/components/Admin/OverviewTable.vue
@@ -13,47 +13,94 @@
             width="100%"
             cellspacing="0"
           >
-            <thead >
-              <tr >
+            <thead>
+              <tr>
                 <th>Title</th>
                 <th>Category</th>
                 <th>On sale</th>
                 <th>Price</th>
-            
+
                 <th>In stock</th>
               </tr>
             </thead>
-            <tbody v-for="item in menuItems " :key="item.id">
+            <tbody v-for="item in menuItems" :key="item.id" >
               <tr>
-                <td> {{ item.title}}</td>
-                <td>{{ item.category}}</td>
-                <td>{{ item.onSale}}</td>
-                <td>${{ item.price}}</td>
-                <td>{{ item.inStock}}x</td>
-                <td> <a class="btn btn-primary btn-user btn-block" >
+                <td>{{ item.title }}</td>
+                <td>{{ item.category }}</td>
+                <td>{{ item.onSale }}</td>
+                <td>${{ item.price }}</td>
+                <td>{{ item.inStock }}x</td>
+                <td>
+                  <a
+                    id="show-btn"
+                    @click="$bvModal.show('bv-modal-example')"
+                    class="btn btn-primary btn-user btn-block"
+                  >
                     Edit
-                  </a></td>
-                <td> <a class="btn btn-primary btn-user btn-block" @click="deleteProduct(item.id)">
+                  </a>
+                   <b-button v-b-modal.modal-center>Launch centered modal</b-button>
+
+                <!--   <EditModal /> -->
+                
+                
+                </td>
+                <td>
+                  <a
+                    class="btn btn-primary btn-user btn-block"
+                    @click="deleteProduct(item.id)"
+                  >
                     Delete
-                  </a></td>
+                  </a>
+                </td>
               </tr>
+              <b-modal id="modal-center" centered title="BootstrapVue">
+    <p class="my-4">
+        <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="In stock"
+            v-model="item.inStock"
+          />
+        <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="In stock"
+            v-model="item.title"
+          />
+        <input
+            type="text"
+            class="form-control form-control-user"
+            id="exampleInputEmail"
+            placeholder="In stock"
+            v-model="item.price"
+          />
+    </p>
+ <b-button @click="updateChanges(item)">Update</b-button>
+                </b-modal>
             </tbody>
           </table>
-          <div >
-             
-          </div>
+          
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import { dbMenuAdd } from "../../firebase.js"
+/* import EditModal from "@/components/Admin/EditModal.vue"; */
+export default {
+  components: {
+/*     EditModal, */
+  },
+  data() {
+    return {
+        item:[],
 
-    export default {
-        computed: {
+    };
+  },
+  computed: {
     menuItems() {
       return this.$store.getters.getMenuItems;
     },
@@ -61,18 +108,16 @@ import { dbMenuAdd } from "../../firebase.js"
   beforeCreate() {
     this.$store.dispatch("setMenuItems");
   },
-  methods:{
-      deleteProduct(id){
-          dbMenuAdd.doc(id).delete().then(() =>{
-              console.log("delete from db")
-
-          })
-      }
-  }
-        
-    }
+  methods: {
+        updateChanges(item) {
+         this.item = item
+      },
+    deleteProduct(id) {
+      this.$store.dispatch("deleteProduct", id);
+    },
+    
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
