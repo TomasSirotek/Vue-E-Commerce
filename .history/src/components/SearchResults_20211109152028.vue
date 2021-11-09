326f386ee88ac9c52b583e6bfe6b2d262e26740e
TomasisBOSS <72190589+TomasisBOSS@users.noreply.github.com>
2021-11-09 16:15:10 +0100

    work on testing functions before

---
+++ .history/src/components/SearchResults_20211109152028.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container p-4 ">
+    <div class="row test">
+      <div class="col-12" >
+        <!-- <div v-for="item in filtered" :key="item.id" > -->
+          <router-link
+            :to="{
+              name: 'productDetails',
+              params: { gameid: this.test.gameID },
+            }"
+          >
+            <ul >
+              <li>{{ item.title }}</li>
+              <li><img :src="item.imageCover" alt="" width="30"></li>
+            </ul>
+          </router-link>
+      <!--   </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* import { mapGetters } from 'vuex'; */
+export default {
+  props:[""],
+  name: "searchResult",
+  data(){
+      return{
+         
+      }
+  },
+  computed: {
+    filtered() {
+    /*  return this.$store.state.games.filter((item) => {
+       return item.title.toLowerCase().includes(this.$store.state.search.toLowerCase())
+     })   */
+     
+        
+     
+      return this.$store.getters.finalFilter; 
+    }
+    /*  ...mapGetters(['finalFilter']) */
+    
+  },
+  methods: {
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.ui-menu{
+    width: 559.984px;
+    top: 81px;
+    left: 672.5px;
+    
+}
+.container{
+        background: wheat;
+    position: absolute;
+    z-index: 2;
+    max-width: 382px;
+}
+
+</style>
