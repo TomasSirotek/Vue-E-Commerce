451fd164f8a96ad298ce87e1f864309fbffc9ecf
TomasisBOSS <72190589+TomasisBOSS@users.noreply.github.com>
2021-11-14 14:54:01 +0100

    front

---
+++ .history/src/components/Admin/TopBar_20211114133733.vue
@@ -0,0 +1,41 @@
+<template>
+  <nav
+    class="
+      navbar navbar-expand navbar-light
+      bg-white
+      topbar
+      mb-4
+      static-top
+      shadow
+    "
+  >
+    <ul class="navbar-nav ml-auto">
+      <div class="d-block mr-4 ">
+         <!-- <b-button v-b-toggle.sidebar-1>
+             <font-awesome-icon icon="cog" class="user" />Admin Panel Panel</b-button>
+ -->
+              <b-button v-b-toggle.sidebar-1 variant="info" 
+                    >Admin Panel</b-button
+                  >
+              <b-button @click="logOut" variant="dark"  class="ml-3"
+                    >  <font-awesome-icon icon="sign-out-alt" class="user" /></b-button
+                  >
+      </div>
+    
+    </ul>
+  </nav>
+</template>
+
+<script>
+import firebase from "firebase/compat";
+export default {
+  methods: {
+    logOut() {
+      firebase.auth().signOut();
+      this.$router.push({ path: "/" });
+    },
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ .history/src/components/MainCardsDisplay_20211114145134.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="product-card">
+    <div class="card">
+      <router-link
+        :to="{
+          name: 'productDetails',
+          params: { gameid: this.cardsSection.gameID },
+        }"
+      >
+        <!-- Product image-->
+        <img class="card-img-top" :src="cardsSection.imageCover" alt="..." />
+        <!-- Product details-->
+        <div class="card-body ">
+          <div class="inner-sticker text-center">
+            <div class="sticker">
+              <div class="category_card mb-2">
+                {{ cardsSection.category }}
+              </div>
+            </div>
+          </div>
+          <div class="text-center p-3" tittle>
+            <!-- Product name-->
+            <h5 class="title">{{ cardsSection.title }}</h5>
+           
+          </div>
+        </div>
+        <!-- Product price-->
+        <div class="card-footer border-top-0">
+          <div class="text-center">
+            <div class="price mt-auto">
+              <span>{{ cardsSection.price }}$</span>
+            </div>
+          </div>
+        </div>
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["cardsSection"],
+  name: "MainCardsDisplay",
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.orange {
+  background: blue;
+}
+</style>
