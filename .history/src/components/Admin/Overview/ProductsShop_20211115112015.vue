58463631152edd799428546fbeb7731709628b60
TomasisBOSS <72190589+TomasisBOSS@users.noreply.github.com>
2021-11-15 11:33:38 +0100

    before merge

---
+++ .history/src/components/Admin/Overview/ProductsShop_20211115112015.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-xl-4 col-md-6 mb-4">
+    <div class="card h-100 py-2">
+      <div class="card-body">
+        <div class="row no-gutters align-items-center">
+          <div class="col mr-2">
+            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+              Products In Shop
+            </div>
+            <div class="row no-gutters align-items-center">
+              <div class="col-auto">
+                <div class="h5 mb-0 mr-3 font-weight-bold">
+                  {{ allGames.length }}
+                </div>
+              </div>
+              <div class="col">
+                <div class="progress progress-sm mr-2">
+                  <div
+                    class="progress-bar bg-info"
+                    role="progressbar"
+                    style="width: 20%"
+                    aria-valuenow="50"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                  ></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+export default {
+  computed: {
+    allGames(){
+          return this.$store.state.games
+    }
+    ...mapState(["games"])
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ .history/src/components/Admin/PhotoPreview_20211115111504.vue
@@ -0,0 +1,19 @@
+<template>
+  <b-modal id="modalPopover" title="Modal with Popover" ok-only>
+    <img :src="this.productPictureName" alt="" />
+  </b-modal>
+</template>
+
+<script>
+import { mapState}
+export default {
+  computed: {
+    productPictureName() {
+      return this.$store.state.productPictureName;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
